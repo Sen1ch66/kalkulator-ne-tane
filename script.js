54b2193a38e18598e90b1cc87fed571a7f469bab
Sen1ch66 <arsenzahovajko@gmail.com>
2024-01-10 16:09:52 +0200

    Готовий калькулятор

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+const xInput = document.querySelector("#x");
+const yInput = document.querySelector("#y");
+const resultInput = document.querySelector("#result");
 const calcbtn = document.querySelector(".kalkulator-btns-btn");
-calcbtn.addEventListener('click', function(){ 
+Number((0.1+0.2).toFixed(1));
+calcbtn.addEventListener('click', function () {
     const x = Number(document.querySelector("#x").value);
     const y = Number(document.querySelector("#y").value);
-    const result = document.querySelector("#result");
     const operacija = document.querySelector("select").value;
     let res = null;
-    if(operacija == "+"){
-        res = x+y
-    } else if(operacija == "-"){
-        res = x-y
-    } else if(operacija == "*"){
-        res = x*y
+    if (operacija == "+") {
+        res = Number((x + y).toFixed(10));
+    } else if (operacija == "-") {
+        res = Number((x - y).toFixed(10));
+    } else if (operacija == "*") {
+        res = Number((x * y).toFixed(10));
     } else {
-        res = x/y
+        res = Number((x / y).toFixed(10));
     }
-    result.value = res;
-    if(x||y == null){
+    if (!x || !y) {
         alert("Введіть 2 числа");
-        result = "";
+        resultInput.value = "";
+    } else {
+        resultInput.value = res;
     }
-
 })
 const clear = document.querySelector(".kalkulator-btns-clear");
-clear.addEventListener('click' ,function(){
-    x.value = "";
-    y.value = "";
-    result.value = "";
+clear.addEventListener('click', function () {
+    xInput.value = "";
+    yInput.value = "";
+    resultInput.value = "";
 })
